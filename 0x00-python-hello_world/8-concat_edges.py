c93883c2f0ea71b2440d81eebb645988d741858e
Valentynszn <valentineogbuagu86@gmail.com>
2024-01-02 09:06:24 -0800

    updated

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+print((("Python is an interpreted, interactive, object-oriented programming"
+        " language that combines remarkable power with very clear syntax")[39:67] +
+       ("Python is an interpreted, interactive, object-oriented programming"
+        " language that combines remarkable power with very clear syntax")[107:112] +
+       ("Python is an interpreted, interactive, object-oriented programming"
+        " language that combines remarkable power with very clear syntax")[:6]))
